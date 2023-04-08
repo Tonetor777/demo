68591130cc44d23cbc65f0de96f71da81685b7da
Tonetor777 <101365568+Tonetor777@users.noreply.github.com>
2023-04-08 06:20:18 -0300

    leap year

---
+++ Leap year .cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std; 
+
+int main () {
+   
+   int year; 
+   cout << "Enter the Year: \n"; 
+   cin >> year; 
+
+   if (year % 4 == 0 ) {
+    cout << "It's a leap year"; 
+   }
+   else cout << "It's not a leap year"; 
+
+
+
+
+
+
+
+
+    return 0; 
+}
